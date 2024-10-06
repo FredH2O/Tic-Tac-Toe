b9cb0f8f37426904ce322ffcec3dbb9e11665aa4
Fred <huertaserick@yahoo.ie>
2024-10-06 17:04:30 +0100

    Training

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,6 +1,13 @@
-function Log() {
-  // array ?
-  return <ol id="log"></ol>;
+function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }
 
 export default Log;
